4195f220cc7258fcc7954e2cd327c23663cac82d
WangShuai0413 <wshuai0413@foxmail.com>
2016-10-27 22:33:54 +0800

    ...

--- python_radis_tasy1.py
+++ python_radis_tasy1.py
@@ -4,27 +4,39 @@ import redis
 r=redis.Redis(host="127.0.0.1",port=6379,db=0)
 
 def sent(key,value):
-    r.set(key,value)
-    return 1
+    if key==None or value==None:
+        return None
+    elif r.set(key,value):
+        return 1
+    else: 
+        return 0
+    
 def printValue(key):
-    value=r.get(key)
-    print 'value:'+value
-    return 1
+    if key:
+        value=r.get(key)
+        if value:
+            return value
+    else:
+        return None
 
 if __name__=='__main__':
     while 1:
+        print '**********************'
         print '1.SET key'
         print '2.GET key'
-        print '0.输入其他键退出'
+        print '0.其他键退出'
+        print '**********************'
         choose=int(input('请输入您的选择:'))
         if choose == 1:
             print '**********SET*********'
             key=raw_input("请输入key：")
             value=raw_input("请输入value:")
             sent(key,value)
+            print '**********************'
         elif choose == 2:
             print '**********GET*********'
             key=raw_input('请输入key:')
-            printValue(key)
+            print printValue(key)
+            print '**********************'
         else:
             exit()
